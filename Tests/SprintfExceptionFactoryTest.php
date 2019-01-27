5e037352b694b34ce8433eb6c0bc7c609c0150f0
SignpostMarv <me@signpostmarv.name>
2019-01-27 14:19:57 +0000

    initial commit

---
+++ Tests/SprintfExceptionFactoryTest.php
@@ -0,0 +1,215 @@
+<?php
+/**
+* @author SignpostMarv
+*/
+declare(strict_types=1);
+
+namespace SignpostMarv\SprintfExceptionFactory\Tests;
+
+use Exception;
+use Generator;
+use InvalidArgumentException;
+use PHPUnit\Framework\TestCase;
+use SignpostMarv\SprintfExceptionFactory\SprintfExceptionFactory;
+use Throwable;
+
+class SprintfExceptionFactoryTest extends TestCase
+{
+    /**
+    * @return array<int, array{0:string, 1:class-string<InvalidArgumentException>, 2:string, 3:array<int, scalar>, 4:int, 5:class-string<Throwable>|null, 6:string, 7:int}>
+    */
+    public function DataProviderInvalidArgumentException() : array
+    {
+        return [
+            [
+                'foo bar',
+                InvalidArgumentException::class,
+                'foo %s',
+                [
+                    'bar',
+                ],
+                0,
+                null,
+                '',
+                0,
+            ],
+        ];
+    }
+
+    public function DataProviderInvalidArgumentExceptionBad() : Generator
+    {
+        foreach ($this->DataProviderInvalidArgumentException() as $args) {
+            $args[1] = Exception::class;
+
+            yield $args;
+        }
+    }
+
+    /**
+    * @param class-string<InvalidArgumentException> $type
+    * @param class-string<Throwable>|null $previousType
+    * @param array<int, scalar> $args
+    *
+    * @dataProvider DataProviderInvalidArgumentException
+    */
+    public function testInvalidArgumentException(
+        string $expectedMessage,
+        string $type,
+        string $sprintf,
+        array $args,
+        int $code = 0,
+        string $previousType = null,
+        string $previousMessage = '',
+        int $previousCode = 0
+    ) {
+        $previous = static::MaybeObtainThrowable($previousType, $previousMessage, $previousCode);
+
+        $result = SprintfExceptionFactory::InvalidArgumentException(
+            $type,
+            $code,
+            $previous,
+            $sprintf,
+            ...$args
+        );
+
+        if (InvalidArgumentException::class !== $type) {
+            static::assertInstanceOf($type, $result);
+        }
+
+        static::PerformAssertions(
+            $result,
+            $previous,
+            $expectedMessage,
+            $type,
+            $sprintf,
+            $args,
+            $code,
+            $previousType,
+            $previousMessage,
+            $previousCode
+        );
+
+        $result = SprintfExceptionFactory::Exception(
+            $type,
+            $code,
+            $previous,
+            InvalidArgumentException::class,
+            $sprintf,
+            ...$args
+        );
+
+        static::assertInstanceOf(InvalidArgumentException::class, $result);
+
+        if (InvalidArgumentException::class !== $type) {
+            static::assertInstanceOf($type, $result);
+        }
+
+        static::PerformAssertions(
+            $result,
+            $previous,
+            $expectedMessage,
+            $type,
+            $sprintf,
+            $args,
+            $code,
+            $previousType,
+            $previousMessage,
+            $previousCode
+        );
+    }
+
+    /**
+    * @param class-string<InvalidArgumentException> $type
+    * @param class-string<Throwable>|null $previousType
+    * @param array<int, scalar> $args
+    *
+    * @dataProvider DataProviderInvalidArgumentExceptionBad
+    */
+    public function testInvalidArgumentExceptionFails(
+        string $expectedMessage,
+        string $type,
+        string $sprintf,
+        array $args,
+        int $code = 0,
+        string $previousType = null,
+        string $previousMessage = '',
+        int $previousCode = 0
+    ) {
+        $previous = static::MaybeObtainThrowable($previousType, $previousMessage, $previousCode);
+
+        static::expectException(InvalidArgumentException::class);
+        static::expectExceptionMessage(
+            'Argument 1 passed to ' .
+            SprintfExceptionFactory::class .
+            '::Exception() must be an implementation of ' .
+            InvalidArgumentException::class .
+            ', ' .
+            $type .
+            ' given!'
+        );
+
+        $result = SprintfExceptionFactory::InvalidArgumentException(
+            $type,
+            $code,
+            $previous,
+            $sprintf,
+            ...$args
+        );
+    }
+
+    /**
+    * @param class-string<Throwable> $type
+    * @param class-string<Throwable>|null $previousType
+    * @param array<int, scalar> $args
+    */
+    protected function PerformAssertions(
+        Throwable $result,
+        Throwable $expectedPrevious = null,
+        string $expectedMessage = '',
+        string $type = Throwable::class,
+        string $sprintf = '%s',
+        array $args = [''],
+        int $code = 0,
+        string $previousType = null,
+        string $previousMessage = '',
+        int $previousCode = 0
+    ) {
+        static::assertSame($expectedMessage, $result->getMessage());
+        static::assertSame($code, $result->getCode());
+
+        $resultPrevious = $result->getPrevious();
+
+        static::assertSame($expectedPrevious, $resultPrevious);
+
+        if ( ! is_null($previousType)) {
+            static::assertInstanceOf($previousType, $resultPrevious);
+        } else {
+            static::assertNull($resultPrevious);
+        }
+
+        if ( ! is_null($expectedPrevious)) {
+            static::assertInstanceOf(Throwable::class, $resultPrevious);
+            static::assertSame($previousMessage, $resultPrevious->getMessage());
+            static::assertSame($previousCode, $resultPrevious->getCode());
+        }
+    }
+
+    /**
+    * @param class-string<Throwable>|null $previousType
+    *
+    * @return Throwable|null
+    */
+    protected static function MaybeObtainThrowable(
+        string $previousType = null,
+        string $previousMessage = '',
+        int $previousCode = 0
+    ) {
+        $previous = null;
+
+        if ( ! is_null($previousType)) {
+            $previous = new $previousType($previousMessage, $previousCode);
+        }
+
+        return $previous;
+    }
+}
---
+++ src/SprintfExceptionFactory.php
@@ -0,0 +1,84 @@
+<?php
+/**
+* @author SignpostMarv
+*/
+declare(strict_types=1);
+
+namespace SignpostMarv\SprintfExceptionFactory;
+
+use Exception;
+use InvalidArgumentException;
+use Throwable;
+
+abstract class SprintfExceptionFactory
+{
+    /**
+    * @param class-string<Exception>|null $type
+    * @param class-string<Exception> $expected
+    * @param scalar ...$args
+    */
+    public static function Exception(
+        string $type = null,
+        int $code = 0,
+        Throwable $previous = null,
+        string $expected = Exception::class,
+        string $sprintf = '%s',
+        ...$args
+    ) : Exception {
+        $type = $type ?: Exception::class;
+
+        if ($type !== $expected && ! is_a($type, $expected, true)) {
+            throw static::ExpectArgumentIsException(
+                $type,
+                $expected,
+                1,
+                __METHOD__,
+                $code,
+                $previous
+            );
+        }
+
+        return new $type(sprintf($sprintf, ...$args));
+    }
+
+    /**
+    * @param class-string<InvalidArgumentException>|null $type
+    * @param scalar ...$args
+    */
+    public static function InvalidArgumentException(
+        string $type = null,
+        int $code = 0,
+        Throwable $previous = null,
+        string $sprintf = '%s',
+        ...$args
+    ) : InvalidArgumentException {
+        return static::Exception(
+            $type ?: InvalidArgumentException::class,
+            $code,
+            $previous,
+            InvalidArgumentException::class,
+            $sprintf,
+            ...$args
+        );
+    }
+
+    public static function ExpectArgumentIsException(
+        string $type,
+        string $expected,
+        int $argument,
+        string $method,
+        int $code = 0,
+        Throwable $previous = null
+    ) : InvalidArgumentException {
+        throw static::InvalidArgumentException(
+            null,
+            $code,
+            $previous,
+            'Argument %u passed to %s() must be an implementation of %s, %s given!',
+            $argument,
+            $method,
+            $expected,
+            $type
+        );
+    }
+}
